09c61f7740e734be8109f66ebc7b1c19e0d4ebe4
Mahtab024 <mahtabalam.bsc786@gmail.com>
2023-08-29 18:48:25 +0530

    Web development project Mini(My self)

---
+++ moonstack/practic/p.js
@@ -0,0 +1,34 @@
+// let x  = "hel\"lo";
+// console.log(x);
+//  let x ="moon";
+//  console.log( typeof x); 
+// let x="moon";
+// alert("Bhai"+ x);
+// let age=10;
+// if(age<=20){
+// console.log("You are Eligible for vote!");
+
+// }
+// else if (age>20){
+// console.log("First Check Your id card ..")
+// }
+// else{
+//     console.log("Nahi de sakte vote....")
+// }
+let age=10;
+let idhai="yes"
+if(age>=17){
+
+    if(idhai=="yes"){
+console.log("You can vote..")
+    }
+    else
+    {
+
+    }
+}
+
+
+
+
+
